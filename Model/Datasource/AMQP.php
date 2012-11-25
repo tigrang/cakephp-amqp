ff700b94cf3cf727174318fd733bbe8ae7219935
Tigran Gabrielyan <tigrangab@gmail.com>
2012-11-24 18:35:22 -0800

    Fixed formatting

--- Model/Datasource/AMQP.php
+++ Model/Datasource/AMQP.php
@@ -1,12 +1,15 @@
 <?php
-
+App::uses('DataSource', 'Model/Datasource');
+/**
+ *
+ */
 class AMQP extends DataSource {
 
-/**
- * The default configuration
- *
- * @var array
- */
+	/**
+	 * The default configuration
+	 *
+	 * @var array
+	 */
 	protected $_baseConfig = array(
 		'host' => 'localhost',
 		'port' => 5672,
@@ -19,38 +22,45 @@ class AMQP extends DataSource {
 		'types' => array('*')
 	);
 
-/**
- * AMQP Connection
- *
- * @var AMQPConnection
- */
+	/**
+	 * AMQP Connection
+	 *
+	 * @var AMQPConnection
+	 */
 	protected $_connection;
 
-/**
- * AMQP Exchange
- *
- * @var AMQPExchange
- */
+	/**
+	 * AMQP Exchange
+	 *
+	 * @var AMQPExchange
+	 */
 	protected $_exchange;
 
-/**
- * Constructor
- *
- * @param array $config
- */
-    function __construct($config = array()) {
-        parent::__construct();
-        $this->setConfig(array_merge($this->_baseConfig, $config));
-        $this->fullDebug = Configure::read('debug') > 1;
-        $this->createConnection();
-        $this->connect();
-    }
+	/**
+	 * AMQP Channel
+	 *
+	 * @var AMQPChannel
+	 */
+	protected $_channel;
 
-/**
- * Creates a connection to the AMQP Server
- *
- * @return void
- */
+	/**
+	 * Constructor
+	 *
+	 * @param array $config
+	 */
+	function __construct($config = array()) {
+		parent::__construct();
+		$this->setConfig(array_merge($this->_baseConfig, $config));
+		$this->fullDebug = Configure::read('debug') > 1;
+		$this->createConnection();
+		$this->connect();
+	}
+
+	/**
+	 * Creates a connection to the AMQP Server
+	 *
+	 * @return void
+	 */
 	public function createConnection() {
 		$credentials = array(
 			'host' => $this->config['host'],
@@ -60,114 +70,122 @@ class AMQP extends DataSource {
 			'password' => $this->config['password'],
 		);
 
-        $this->_connection = new AMQPConnection($credentials);
+		$this->_connection = new AMQPConnection($credentials);
 	}
 
-/**
- * Connects to the RabbitMQ Server
- *
- * @return bool
- */
+	/**
+	 * Connects to the AMQP Server
+	 *
+	 * @return bool
+	 */
 	public function connect() {
-        if ($this->isConnected()) {
-            return true;
-        }
-        
-        try {
-		    if (!$this->_connection->connect()) {
-                return false;
-            }
+		if ($this->isConnected()) {
+			return true;
+		}
 
-            $this->_exchange = new AMQPExchange($this->_connection);
-		    $this->_exchange->declare(
-                $this->config['exchange'],
-                $this->config['exchangeType'],
-                $this->config['exchangeFlags']
-            );
-        } catch (Exception $e) {
-            throw new CakeException("RabbitMQ Datasource - " . $e->getMessage());
-        }
+		try {
+			if (!$this->_connection->connect()) {
+				return false;
+			}
 
-        return true;
+			$this->_channel = new AMQPChannel($this->_connection);
+			$this->_exchange = new AMQPExchange($this->_channel);
+			$this->_exchange->setName($this->config['exchange']);
+			$this->_exchange->setType($this->config['exchangeType']);
+			$this->_exchange->setFlags($this->config['exchangeFlags']);
+			$this->_exchange->declare();
+		} catch (Exception $e) {
+			throw new CakeException("AMQP Datasource - " . $e->getMessage());
+		}
+
+		return true;
 	}
-    
-/**
- * Disconnects the connection
- *
- * @return bool
- */
-    function disconnect() {
-        if ($this->isConnected()) {
-            return $this->_connection->disconnect();
-        }
 
-        return true;
-    }
+	/**
+	 * Disconnects the connection
+	 *
+	 * @return bool
+	 */
+	function disconnect() {
+		if ($this->isConnected()) {
+			return $this->_connection->disconnect();
+		}
 
-/**
- * Returns the connection status
- *
- * @return bool
- */
-    public function isConnected() {
-        if (!$this->_connection) {
-            return false;
-        }
-        
-        return $this->_connection->isConnected();
-    }
+		return true;
+	}
 
-/**
- * @param string  $type
- * @param mixed $message
- * @return bool
- */
-	public function publish($type, $message, $eta = null) {
-        $this->connect();
-        
-		if (!in_array($type, $this->config['types']) &&
-            $this->config['types'] !== array('*')) {
+	/**
+	 * Returns the connection status
+	 *
+	 * @return bool
+	 */
+	public function isConnected() {
+		if (!$this->_connection) {
 			return false;
 		}
 
-        if (is_array($message)) {
-            $message = json_encode($message);
-        }
-
-		$result = $this->_exchange->publish($message, $type);
-        $this->disconnect();
-
-        return $result;
+		return $this->_connection->isConnected();
 	}
 
-/**
- * All calls to methods on the model are routed through this method
- *
- * @param mixed $method
- * @param mixed $params
- * @param mixed $Model
- * @access public
- * @return boolean
- */
-    public function query($method, $params, &$Model) {
-        $startQuery = microtime(true);
-        switch ($method) {
-            case 'publish':
-                $result = $this->dispatchMethod($method, $params);
-                return $result;
-            default:
-                trigger_error("RabbitMQ::query - Unkown method {$method}.", E_USER_WARNING);
-                return false;
-        }
-    }
+	/**
+	 * @param string $type
+	 * @param string|array $message
+	 * @return bool
+	 */
+	public function publish($message, $routingKey, $flags = AMQP_NOPARAM, $attributes = []) {
+		$this->connect();
 
-/**
- * Returns available sources
- *
- * @see Mode::useTable
- * @return array
- */
-    function listSources($data = null) {
-        return array('tasks');
-    }
+		if (!in_array($routingKey, $this->config['types']) && $this->config['types'] !== array('*')) {
+			return false;
+		}
+
+		if (is_array($message)) {
+			$message = json_encode($message);
+		}
+
+		$result = null;
+		try {
+			$result = $this->_exchange->publish($message, $routingKey, $flags, $attributes);
+		} catch (AMQPExchangeException $e) {
+
+		} catch (AMQPChannelException $e) {
+
+		} catch (AMQPConnectionException $e) {
+
+		}
+
+		$this->disconnect();
+		return $result;
+	}
+
+	/**
+	 * All calls to methods on the model are routed through this method
+	 *
+	 * @param mixed $method
+	 * @param mixed $params
+	 * @param mixed $Model
+	 * @access public
+	 * @return boolean
+	 */
+	public function query($method, $params, &$Model) {
+		switch ($method) {
+			case 'publish':
+				$result = $this->dispatchMethod($method, $params);
+				return $result;
+			default:
+				trigger_error("RabbitMQ::query - Unknown method {$method}.", E_USER_WARNING);
+				return false;
+		}
+	}
+
+	/**
+	 * Returns available sources
+	 *
+	 * @see Mode::useTable
+	 * @return array
+	 */
+	function listSources($data = null) {
+		return array('tasks');
+	}
+
 }
